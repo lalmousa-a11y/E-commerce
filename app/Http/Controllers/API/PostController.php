422fa0d26fa4db0253429bf0b67f9d857d4fae48
Lama Almousa <lalmousa@gathern.com>
2025-12-18 16:49:59 +0300

    sellers mangment v2

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\SellerResource;
 use App\Models\Post;
+use App\Models\Seller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $users = User::where('user_type', 'seller')->get();
+        return SellerResource::collection($users);    
         return response()->json(Post::with('user')->latest()->get());
     }
     public function store(Request $request)
@@ -38,4 +43,31 @@ class PostController extends Controller
         $post->delete();
         return response()->json(['message' => 'Post deleted']);
     }
+
+    public function approve($user_id)
+    {
+        $seller = Seller::where("user_id", $user_id)->first();
+
+        if($seller != null){
+            $seller->is_approved = 1;
+            $seller->save();
+            return response()->json(['message' => 'Seller approved successfully']);
+        }
+
+        return response()->json(['message' => 'Seller not found'], 404);
+    }
+
+    public function disapprove($user_id)
+    {
+        $seller = Seller::where("user_id", $user_id)->first();
+
+        if($seller != null){
+            $seller->is_approved = 0;
+            $seller->save();
+            return response()->json(['message' => 'Seller disapproved successfully']);
+        }
+
+        return response()->json(['message' => 'Seller not found'], 404);
+    }
+ 
 }
---
+++ app/Http/Resources/SellerResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SellerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */ 
+       public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'is_approved' => $this->seller?->is_approved
+
+        ];
+    }
+}
