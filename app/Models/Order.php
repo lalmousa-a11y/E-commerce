5427232c6a3939328faa2c4ac2e26401af3a44fb
Lama Almousa <lalmousa@gathern.com>
2026-01-13 14:30:25 +0300

    orderV5

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 
 class Order extends Model
 {
@@ -16,5 +18,9 @@ class Order extends Model
     public function items()
     {
         return $this->hasMany(OrderItem::class);
+    }
+       public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
